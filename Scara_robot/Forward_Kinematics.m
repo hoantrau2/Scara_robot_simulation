8e58002028d1044e5383151c5ee3e059c2cd9213
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2023-10-29 10:26:11 +0700

    show orientation

--- Scara_robot/Forward_Kinematics.m
+++ Scara_robot/Forward_Kinematics.m
@@ -13,8 +13,8 @@ handles.Pos_X.String = num2str(round(T(1,4,4),2));
 handles.Pos_Y.String = num2str(round(T(2,4,4),2));
 handles.Pos_Z.String = num2str(round(T(3,4,4),2));
 
-handles.Roll_val.String  = num2str(round(atan2(-T(2,3,4),T(3,3,4))*180/pi,2));
-handles.Pitch_val.String = num2str(round(asin(abs(T(1,3,4)))*180/pi,2));
-handles.Yaw_val.String   = num2str(round(atan2(T(1,2,4),T(1,1,4))*180/pi,2));
+handles.Roll_value.String  = num2str(round(atan2(-T(2,3,4),T(3,3,4))*180/pi,2));
+handles.Pitch_value.String = num2str(round(asin(abs(T(1,3,4)))*180/pi,2));
+handles.Yaw_value.String   = num2str(round(atan2(T(1,2,4),T(1,1,4))*180/pi,2));
  
 end
