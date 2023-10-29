9468eba8c330542f43489aad7b996edf3081aceb
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2023-10-29 10:15:01 +0700

    add some appearances

--- Scara_robot/plot_a1.m
+++ Scara_robot/plot_a1.m
@@ -8,8 +8,8 @@ function plot_a1(handles,T,a,alpha,d,theta)
     opacity = str2double(handles.Opac_val.String);
     Yaw = theta(1)-pi/2;
 
-    fill3(handles.axes1,[x-W/2*cos(Yaw),x+W/2*cos(Yaw),x-L*sin(Yaw)+W/2*cos(Yaw),x-L*sin(Yaw)-W/2*cos(Yaw)],[y-W/2*sin(Yaw), y+W/2*sin(Yaw), y+L*cos(Yaw)+W/2*sin(Yaw), y+L*cos(Yaw)-W/2*sin(Yaw)],[H2, H2, H2, H2 ],[80, 140, 91]/255,'FaceAlpha',opacity)
-    fill3(handles.axes1,[x-W/2*cos(Yaw),x+W/2*cos(Yaw),x-L*sin(Yaw)+W/2*cos(Yaw),x-L*sin(Yaw)-W/2*cos(Yaw)],[y-W/2*sin(Yaw), y+W/2*sin(Yaw), y+L*cos(Yaw)+W/2*sin(Yaw), y+L*cos(Yaw)-W/2*sin(Yaw)],[H1, H1, H1, H1 ],[80, 140, 91]/255,'FaceAlpha',opacity)
-    fill3(handles.axes1,[x+W/2*cos(Yaw), x+W/2*cos(Yaw)-L*sin(Yaw), x+W/2*cos(Yaw)-L*sin(Yaw), x+W/2*cos(Yaw)],[y+W/2*sin(Yaw), y+L*cos(Yaw)+W/2*sin(Yaw), y+L*cos(Yaw)+W/2*sin(Yaw), y+W/2*sin(Yaw)],[H2, H2, H1, H1 ],[80, 140, 91]/255,'FaceAlpha',opacity)
-    fill3(handles.axes1,[x-W/2*cos(Yaw), x-W/2*cos(Yaw)-L*sin(Yaw), x-W/2*cos(Yaw)-L*sin(Yaw), x-W/2*cos(Yaw)],[y-W/2*sin(Yaw), y+L*cos(Yaw)-W/2*sin(Yaw), y+L*cos(Yaw)-W/2*sin(Yaw), y-W/2*sin(Yaw)],[H2, H2, H1, H1 ],[80, 140, 91]/255,'FaceAlpha',opacity)
+    fill3(handles.axes1,[x-W/2*cos(Yaw),x+W/2*cos(Yaw),x-L*sin(Yaw)+W/2*cos(Yaw),x-L*sin(Yaw)-W/2*cos(Yaw)],[y-W/2*sin(Yaw), y+W/2*sin(Yaw), y+L*cos(Yaw)+W/2*sin(Yaw), y+L*cos(Yaw)-W/2*sin(Yaw)],[H2, H2, H2, H2 ],[255, 140, 200]/255,'FaceAlpha',opacity)
+    fill3(handles.axes1,[x-W/2*cos(Yaw),x+W/2*cos(Yaw),x-L*sin(Yaw)+W/2*cos(Yaw),x-L*sin(Yaw)-W/2*cos(Yaw)],[y-W/2*sin(Yaw), y+W/2*sin(Yaw), y+L*cos(Yaw)+W/2*sin(Yaw), y+L*cos(Yaw)-W/2*sin(Yaw)],[H1, H1, H1, H1 ],[255, 140, 200]/255,'FaceAlpha',opacity)
+    fill3(handles.axes1,[x+W/2*cos(Yaw), x+W/2*cos(Yaw)-L*sin(Yaw), x+W/2*cos(Yaw)-L*sin(Yaw), x+W/2*cos(Yaw)],[y+W/2*sin(Yaw), y+L*cos(Yaw)+W/2*sin(Yaw), y+L*cos(Yaw)+W/2*sin(Yaw), y+W/2*sin(Yaw)],[H2, H2, H1, H1 ],[255, 140, 200]/255,'FaceAlpha',opacity)
+    fill3(handles.axes1,[x-W/2*cos(Yaw), x-W/2*cos(Yaw)-L*sin(Yaw), x-W/2*cos(Yaw)-L*sin(Yaw), x-W/2*cos(Yaw)],[y-W/2*sin(Yaw), y+L*cos(Yaw)-W/2*sin(Yaw), y+L*cos(Yaw)-W/2*sin(Yaw), y-W/2*sin(Yaw)],[H2, H2, H1, H1 ],[255, 140, 200]/255,'FaceAlpha',opacity)
 end
--- Scara_robot/plot_arm.m
+++ Scara_robot/plot_arm.m
@@ -11,9 +11,9 @@ th = 0:pi/100:2*pi;
 a = R1*cos(th);
 b = R1*sin(th);
 
-surf(handles.axes1, [a;a]+x, [b;b]+y, [ones(1, size(th, 2)); zeros(1, size(th, 2))]*H1, 'FaceColor',[139 137 137]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
-fill3(handles.axes1, a+x, b+y, z*ones(1, size(th, 2)),[139 137 137]/255, 'FaceAlpha', opacity);
-fill3(handles.axes1, a+x, b+y, H1*ones(1, size(th, 2)),[139 137 137]/255, 'FaceAlpha', opacity);
+surf(handles.axes1, [a;a]+x, [b;b]+y, [ones(1, size(th, 2)); zeros(1, size(th, 2))]*H1, 'FaceColor',[0 0 0]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
+fill3(handles.axes1, a+x, b+y, z*ones(1, size(th, 2)),[0 0 0]/255, 'FaceAlpha', opacity);
+fill3(handles.axes1, a+x, b+y, H1*ones(1, size(th, 2)),[0 0 0]/255, 'FaceAlpha', opacity);
 
 %%
 %D1
@@ -39,13 +39,13 @@ opacity = str2double(handles.Opac_val.String);
 x = r.*cos(u).*sin(v);
 y = r.*sin(u).*sin(v);
 z = r.*cos(v)+363;
-surf(handles.axes1,x,y,z,'EdgeColor',[205 200 177]/255,'FaceColor','none','FaceAlpha',opacity)
+ surf(handles.axes1,x,y,z,'EdgeColor',[05 0 0]/255,'FaceColor','none','FaceAlpha',opacity)
 hold on
 [u,v] = meshgrid(linspace(0,2*pi,50),linspace(acos(20/r),acos(-20/r),50));
 x = r.*cos(u).*sin(v);
 y = r.*sin(u).*sin(v);
 z = r.*cos(v)+d(1);
-surf(handles.axes1,x,y,z,'EdgeColor',[80, 140, 91]/255,'FaceColor','none','FaceAlpha',opacity)
+surf(handles.axes1,x,y,z,'EdgeColor',[255, 140, 200]/255,'FaceColor','none','FaceAlpha',opacity)
 
 %%
 %D2
@@ -61,26 +61,26 @@ a = R1*cos(th);
 b = R1*sin(th);
 
 surf(handles.axes1, [a;a]+x, [b;b]+y, [H1*ones(1, size(th, 2)); ones(1, size(th, 2))*(H1+20)], 'FaceColor',[0 178 238]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
-surf(handles.axes1, [a;a]+x, [b;b]+y, [(z-20)*ones(1, size(th, 2)); ones(1, size(th, 2))*(z+20)], 'FaceColor',[80, 140, 91]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
+surf(handles.axes1, [a;a]+x, [b;b]+y, [(z-20)*ones(1, size(th, 2)); ones(1, size(th, 2))*(z+20)], 'FaceColor',[255, 140, 200]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
 surf(handles.axes1, [a;a]+x, [b;b]+y, [(z+20)*ones(1, size(th, 2)); ones(1, size(th, 2))*(z+d(2)-20)], 'FaceColor',[0 178 238]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
 % fill3(handles.axes1, a+x, b+y, H2*ones(1, size(th, 2)),[0 178 238]/255, 'FaceAlpha', opacity);
 fill3(handles.axes1, a+x, b+y, H1*ones(1, size(th, 2)),[0 178 238]/255, 'FaceAlpha', opacity);
 
 %%
 %Joint 2
-r= 50;
+r= 70;
 opacity = str2double(handles.Opac_val.String);
 [u,v] = meshgrid(linspace(0,2*pi,50),linspace(0,acos(20/r),50));
 x = r.*cos(u).*sin(v) + round(T(1,4,1),2);
 y = r.*sin(u).*sin(v) + round(T(2,4,1),2);
-z = r.*cos(v) + d(1) + d(2);
-surf(handles.axes1,x,y,z,'EdgeColor',[205 200 177]/255,'FaceColor','none','FaceAlpha',opacity)
+z = r.*cos(v) + d(1) + d(2)+40;
+%  surf(handles.axes1,x,y,z,'EdgeColor',[0 0 0]/255,'FaceColor','none','FaceAlpha',opacity)
 hold on
-[u,v] = meshgrid(linspace(0,2*pi,50),linspace(acos(20/r),acos(-20/r),50));
-x = r.*cos(u).*sin(v) + round(T(1,4,1),2);
+[u,v] = meshgrid(linspace(0,2*pi,70),linspace(acos(70/r),acos(-20/r),70));
+x = r.*cos(u).*sin(v) + round(T(1,4,1),2) ;
 y = r.*sin(u).*sin(v) + round(T(2,4,1),2);
 z = r.*cos(v) + d(1) + d(2);
-surf(handles.axes1,x,y,z,'EdgeColor',[80, 140, 91]/255,'FaceColor','none','FaceAlpha',opacity)
+surf(handles.axes1,x,y,z,'EdgeColor',[0, 0, 0]/255,'FaceColor','none','FaceAlpha',opacity)
 
 %%
 %D3
@@ -113,7 +113,7 @@ a = R1*cos(th);
 b = R1*sin(th);
 
 surf(handles.axes1, [a;a]+x, [b;b]+y, [H1*ones(1, size(th, 2)); ones(1, size(th, 2))*H3], 'FaceColor',[255 165 79]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
-surf(handles.axes1, [a;a]+x, [b;b]+y, [H1*ones(1, size(th, 2)); ones(1, size(th, 2))*H2], 'FaceColor',[80 140 91]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
+surf(handles.axes1, [a;a]+x, [b;b]+y, [H1*ones(1, size(th, 2)); ones(1, size(th, 2))*H2], 'FaceColor',[255, 140, 200]/255, 'EdgeColor', 'none', 'FaceAlpha', opacity)
 fill3(handles.axes1, a+x, b+y, H1*ones(1, size(th, 2)),'black', 'FaceAlpha', opacity);
  fill3(handles.axes1, a+x, b+y, z*ones(1, size(th, 2)),'black', 'FaceAlpha', opacity);
 
